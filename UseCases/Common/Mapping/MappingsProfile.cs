4d1777150b93ec9dcae6251e53ebe955382e4479
Alejandro <alejandro.rangel.avl@gmail.com>
2025-10-03 15:25:23 -0600

    Generacion automatizada de clases base para entidades nuevas

--- UseCases/Common/Mapping/MappingsProfile.cs
+++ UseCases/Common/Mapping/MappingsProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Domain.Entities;
 using DTO.Catalog;
+using DTO.CatalogItem;
 
 namespace UseCases.Common.Mapping;
 
@@ -8,6 +9,7 @@ public class MappingsProfile : Profile
 {
     public MappingsProfile()
     {
+        CreateMap<Domain.Entities.CatalogItem, DTO.CatalogItem.CatalogItemDTO>().ReverseMap();
         CreateMap<Catalog, CatalogDTO>().ReverseMap();
     }
 }
