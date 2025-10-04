213c1b102c618f2c96be1bb5c77a6dd85783f213
Alejandro <alejandro.rangel.avl@gmail.com>
2025-10-03 20:27:05 -0600

    primer entidad completa

--- Validator/CatalogItemDTOValidator.cs
+++ Validator/CatalogItemDTOValidator.cs
@@ -7,11 +7,6 @@ public class CatalogItemDTOValidator : AbstractValidator<CatalogItemDTO>
 {
     public CatalogItemDTOValidator()
     {
-        // TODO: Agrega las reglas de validación específicas para CatalogItemDTO.
-        // Ejemplo:
-        // RuleFor(x => x.Code).NotEmpty().WithMessage("El campo Code es requerido");
-        // RuleFor(x => x.Name).NotEmpty().WithMessage("El campo Name es requerido");
-        
         RuleFor(x => x.CatalogId).NotNull();
     }
 }
