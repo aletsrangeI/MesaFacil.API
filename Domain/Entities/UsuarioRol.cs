0da60c6391f0056d3a7fb5203e4c7033266fef51
Alejandro <alejandro.rangel.avl@gmail.com>
2025-10-20 16:32:30 -0600

    se cambia el cors

--- Domain/Entities/UsuarioRol.cs
+++ Domain/Entities/UsuarioRol.cs
@@ -3,7 +3,7 @@ namespace Domain.Entities;
 public class UsuarioRol : BaseAuditableEntity
 {
     public int IdRol { get; set; }
-
+    public int UsuarioId { get; set; }
     public Usuario Usuario { get; set; } = null!;
     public Rol Rol { get; set; } = null!;
 }
